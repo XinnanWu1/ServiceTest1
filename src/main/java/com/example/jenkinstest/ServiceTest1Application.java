fd01032a66b7c4950fa500f1c71b24c20c6a0cda
wuxinnan <3495518091@qq.com>
2026-07-06 17:02:35 +0800

    初始化

---
+++ src/main/java/com/example/jenkinstest/ServiceTest1Application.java
@@ -0,0 +1,13 @@
+package com.example.jenkinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceTest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceTest1Application.class, args);
+    }
+
+}
